9e2dbb51eb5f540b772b89577876c615c0241bf1
Jey-Pee.dev <jotadaysmed@gmail.com>
2024-02-16 08:04:29 -0500

    Add files via upload

---
+++ cavsurBackend/src/test/java/com/cavsur/cavsurBackend/CavsurBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavsur.cavsurBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CavsurBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
